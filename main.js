6759491e5048cf7a730e9718e29d5eb19be1bfda
JoeDaHippie <plnicholas10@gmail.com>
2022-11-09 21:13:39 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//Mixed Messages Portfolio Project for Codecademy.  Generates random message on each run of the program
+const targets = ['monkey', 'man', 'elephant', 'car', 'toad', 'frog', 'mouse', 'rat', 'cow', 'pencil', 'notebook', 'window', 'elevator', 'tow truck',
+               'AC unit', 'bear', 'building', 'jackhammer', 'TV', 'cabinet', 'scurge of the Earth', 'Satan himself', 'Jimbo Swackhammer', 
+               'Lebron James', 'John Cena', 'Dwane "The Rock" Johnson', 'room full of hungry seagulls', 'alcoholic', 'incel', 'scammer', 'gays'];
+const targetLength = part1.length;
